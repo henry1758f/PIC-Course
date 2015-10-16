9f73cf78bd7a84148310ca8eb687f914af2a9144
henry1758f <henry1758f@gmail.com>
2015-10-16 19:53:01 +0800

    20151016_Create HW4
    作業專案建立

---
+++ mcu_12_4.X/newmainXC16.c
@@ -0,0 +1,15 @@
+/*
+ * File:   newmainXC16.c
+ * Author: TEAM 12
+ * A/D Converter Homework / MCU-Course HW4 / KUAS 
+ * Created on 2015年10月16日, 下午 7:43
+ */
+
+#include "xc.h"
+#include <adc.h>
+
+int main(void) 
+{
+    
+    return 0;
+}
